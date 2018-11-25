df95ead61b3969b86058f78459e0177f452217bd
Rami Mustaklem <ramimustaklem@gmail.com>
2018-11-26 01:28:22 +0200

    End of lesson 10 - Setup add store form and post data

--- starter-files/controllers/storeController.js
+++ starter-files/controllers/storeController.js
@@ -4,3 +4,15 @@ exports.homePage = (req, res) => {
         title: 'Page Title'
     });
 };
+
+exports.addStore = (req, res) => {
+
+    res.render('editStore', {
+        title: 'Add Store'
+    });
+};
+
+exports.createStore = (req, res) => {
+
+    res.json(req.body);
+};
