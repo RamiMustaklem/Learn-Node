b96903e66b3d1183709e6d3f0ba0f3c849526a1d
Rami Mustaklem <ramimustaklem@gmail.com>
2018-12-23 16:21:37 +0200

    End of lesson number 20

--- starter-files/models/Store.js
+++ starter-files/models/Store.js
@@ -35,7 +35,7 @@ const storeSchema = new mongoose.Schema({
     photo: String
 });
 
-storeSchema.pre('save', function (next) {
+storeSchema.pre('save', async function (next) {
 
     if (!this.isModified('name')) {
         next(); // skip making a slug
@@ -43,6 +43,14 @@ storeSchema.pre('save', function (next) {
     }
 
     this.slug = slug(this.name);
+    // find other store that have a slug of the same name slug, slug-1, slug-2
+    const slugRegEx = new RegExp(`^(${this.slug})((-[0-9]*$)?)$`, 'i');
+    const storesWithSlug = await this.constructor.find({ slug: slugRegEx });
+
+    if (storesWithSlug.length) {
+        this.slug = `${this.slug}-${storesWithSlug.length + 1}`;
+    }
+
     next();
 });
 
