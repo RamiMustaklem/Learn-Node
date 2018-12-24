02940f5999245f9f51dd062d3aeb1992537303bc
Rami Mustaklem <ramimustaklem@gmail.com>
2018-12-24 13:43:44 +0200

    End of lesson number 23

--- starter-files/routes/index.js
+++ starter-files/routes/index.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const router = express.Router();
 const storeController = require('../controllers/storeController');
+const userController = require('../controllers/userController');
 const { catchErrors } = require('../handlers/errorHandlers');
 
 // Do work here
@@ -29,5 +30,15 @@ router.get('/reverse/:name', (req, res) => {
 
 router.get('/tags', catchErrors(storeController.getStoresByTag));
 router.get('/tags/:tag', catchErrors(storeController.getStoresByTag));
+
+router.get('/login', userController.loginForm);
+router.get('/register', userController.registerForm);
+
+// 1. Validate the registration data
+// 2. register the user
+// 3. we need to log them in
+router.post('/register',
+
+    userController.validateRegister);
 
 module.exports = router;
