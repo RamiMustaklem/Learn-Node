816bb6de2186b241fc4da17e888d116db5f4ddac
Rami Mustaklem <ramimustaklem@hotmail.com>
2019-03-18 23:57:21 +0200

    End of lesson number 38

--- starter-files/models/Store.js
+++ starter-files/models/Store.js
@@ -38,6 +38,9 @@ const storeSchema = new mongoose.Schema({
         ref: 'User',
         required: 'You must supply an author'
     }
+}, {
+    toJSON: { virtuals: true },
+    toObject: { virtuals: true },
 });
 
 storeSchema.index({
@@ -73,5 +76,12 @@ storeSchema.statics.getTagsList = function () {
         { $sort: { count: -1 } }
     ]);
 };
+
+// find reviews where the stores _id property === reviews store property
+storeSchema.virtual('reviews', {
+    ref: 'Review', // what model to link?
+    localField: '_id', // which field on the store?
+    foreignField: 'store' // which field on the review?
+});
 
 module.exports = mongoose.model('Store', storeSchema);
